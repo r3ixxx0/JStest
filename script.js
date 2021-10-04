6179a38b13f8ed772d187c7a64454be6d4135911
r3ixxx0 <86443757+r3ixxx0@users.noreply.github.com>
2021-10-04 15:48:16 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-alert('Seja bem-vindo') //O alert cria uma caixa de texto na pagina 
-        confirm('Deseja fazer o cadastro?') //O confirm cria uma caixa de confirmação na pagina
-        var nome = prompt('Insira seu nome') //O var armazena essa informação (variavel), o "=" significa recebe, o prompt cria uma caixa de texto onde você insere os dados 
-        var sobrenome = prompt('Insira seu sobrenome') 
-        var idade = prompt('Insira sua idade')
-        document.write(`Ola ${nome}`)
-        document.write(` ${sobrenome}, `)
-        document.write(`${idade} anos`)
+window.onload = function() {
+    alert('Seja bem-vindo') //O alert cria uma caixa de texto na pagina 
+    var confirmacao = confirm('Deseja fazer o cadastro?') //O confirm cria uma caixa de confirmação na pagina
+    if(confirmacao){
+        let nome = prompt('Insira seu nome') //O var armazena essa informação (variavel), o "=" significa recebe, o prompt cria uma caixa de texto onde você insere os dados 
+        let sobrenome = prompt('Insira seu sobrenome') 
+        let idade = parseFloat(prompt('Insira sua idade'))
+        let elemento = document.querySelector('#main')
         if (idade >=18){
             alert('Seja bem vindo')
+            elemento.innerText = `Olá ${nome} ${sobrenome}!! Seja bem vindo!`
         }
-        if (idade < 18){
-            alert('Atençaõ somente maiores de 18 anos são permitidos nesse site')
+        if (idade <18){
+            elemento.innerText = `Olá ${nome} ${sobrenome}!! Verificamos que você é novo demais para acessar nosso site. Lamento :(`
         }
+    }
+
+    console.log(nome, sobrenome, idade)
+} 
+        
